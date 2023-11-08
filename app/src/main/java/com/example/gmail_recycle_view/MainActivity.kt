bd57ae9b10b7306dd962c00d9b6842e51f7d54c3
Viet Anh <buivietanh399@gmail.com>
2023-11-08 16:49:29 +0700

    commit

---
+++ app/src/main/java/com/example/gmail_recycle_view/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gmail_recycle_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val data  = ArrayList<ItemViewModel>()
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+        data.add(ItemViewModel("Mệt ghê", "うち", "やすいですね", "10:00PM"))
+
+        val adapter = ItemAdapter(data)
+        recyclerView.adapter = adapter
+    }
+}
